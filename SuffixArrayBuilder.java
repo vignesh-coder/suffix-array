9cb30edac1ea1e40fe88d0b15091af7272c4f639
Vignesh <31616369+vignesh-coder@users.noreply.github.com>
2017-09-04 17:42:13 +0530

    Update SuffixArrayBuilder.java

--- SuffixArrayBuilder.java
+++ SuffixArrayBuilder.java
@@ -13,43 +13,43 @@ class SuffixArrayBuilder{
 	}
 	static int[] buildSuffixArray(String str){
 		int n = str.length();
-		Suffix suffices[] = new Suffix[n];
+		Suffix suffixes[] = new Suffix[n];
 		for(int i = 0;i<n;i++){
-			suffices[i] = new Suffix();
-			suffices[i].index = i;
-			suffices[i].rank[0] = str.charAt(i)-'a';
-			suffices[i].rank[1] = ((i+1)<n)?(str.charAt(i+1)-'a'):-1;
+			suffixes[i] = new Suffix();
+			suffixes[i].index = i;
+			suffixes[i].rank[0] = str.charAt(i)-'a';
+			suffixes[i].rank[1] = ((i+1)<n)?(str.charAt(i+1)-'a'):-1;
 		}
-		Arrays.sort(suffices);
+		Arrays.sort(suffixes);
 		
 		int ind[] = new int[n];
 		for(int k = 4;k<2*n;k*=2){
 			int rank = 0;
-			int prev_rank = suffices[0].rank[0];
-			suffices[0].rank[0] = rank;
-			ind[suffices[0].index] = 0;
+			int prev_rank = suffixes[0].rank[0];
+			suffixes[0].rank[0] = rank;
+			ind[suffixes[0].index] = 0;
 			for(int i = 1;i<n;i++){
 				
-				if(suffices[i].rank[0] == prev_rank && suffices[i].rank[1] == suffices[i-1].rank[1]){
-					prev_rank = suffices[i].rank[0];
-					suffices[i].rank[0] = rank;
+				if(suffixes[i].rank[0] == prev_rank && suffixes[i].rank[1] == suffixes[i-1].rank[1]){
+					prev_rank = suffixes[i].rank[0];
+					suffixes[i].rank[0] = rank;
 				}
 				else{
-					prev_rank = suffices[i].rank[0];
-					suffices[i].rank[0] = ++rank;
+					prev_rank = suffixes[i].rank[0];
+					suffixes[i].rank[0] = ++rank;
 				}
 				
-				ind[suffices[i].index] = i;
+				ind[suffixes[i].index] = i;
 			}
 			for (int i = 0;i<n;i++) {
-				int nextIndex = suffices[i].index+k/2;
-				suffices[i].rank[1] = (nextIndex<n)?suffices[ind[nextIndex]].rank[0]:-1;
+				int nextIndex = suffixes[i].index+k/2;
+				suffixes[i].rank[1] = (nextIndex<n)?suffixes[ind[nextIndex]].rank[0]:-1;
 			}
-			Arrays.sort(suffices);
+			Arrays.sort(suffixes);
 		}
 		int suffixArray[] = new int[n];
 		for(int i = 0;i<n;i++)
-			suffixArray[i] = suffices[i].index;
+			suffixArray[i] = suffixes[i].index;
 		return suffixArray;
 	}
 	//driver method
